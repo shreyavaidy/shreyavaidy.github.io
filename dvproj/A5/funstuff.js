2a3707cbb287b36021f152a2830fd0f2e0076e39
Shreya Vaidyanathan <shreyavaidy@gmail.com>
2018-03-15 15:39:42 -0400

    extending to my logic
    Assignment logic applied

--- dvproj/A5/funstuff.js
+++ dvproj/A5/funstuff.js
@@ -1,13 +1,17 @@
-var movers = [];
+// Demonstration of multiple force acting on bodies (Mover class)
+//Here forces have been reinterpreted as the hour and minute hitting them
+
+var hands = [];
 
 // Liquid
 var liquid;
 
 function setup() {
-  createCanvas(1200, 650);
+  createCanvas(1200, 500);
   reset();
-  // Create liquid object
-  liquid = new Liquid(0, height/4, width/3, height/4, 0.1);
+  // Create liquid object - treat like an hour bubble of filter through which hands enter and start moving slower
+  liquid = new Liquid(width/3, height/2, width/2, height/2, 0.1);
+  frameRate(30);
 }
 
 function draw() {
@@ -17,25 +21,25 @@ function draw() {
   liquid.display();
   fill("#ccaaa2");
 
-  for (var i = 0; i < movers.length; i++) {
+  for (var i = 0; i < hands.length; i++) {
 
     // Is the Mover in the liquid?
-    if (liquid.contains(movers[i])) {
+    if (liquid.contains(hands[i])) {
       // Calculate drag force
-      var dragForce = liquid.calculateDrag(movers[i]);
+      var dragForce = liquid.calculateDrag(hands[i]);
       // Apply drag force to Mover
-      movers[i].applyForce(dragForce);
+      hands[i].applyForce(dragForce);
     }
 
     // Gravity is scaled by mass here!
-    var gravity = createVector(0, 0.1*movers[i].mass);
+    var gravity = createVector(0, 0.1*hands[i].mass);
     // Apply gravity
-    movers[i].applyForce(gravity);
+    hands[i].applyForce(gravity);
 
     // Update and display
-    movers[i].update();
-    movers[i].display();
-    movers[i].checkEdges();
+    hands[i].update();
+    hands[i].display();
+    hands[i].checkEdges();
   }
 
 }
@@ -45,10 +49,10 @@ function mousePressed() {
   reset();
 }
 
-// Restart all the Mover objects randomly
+// Restart all the hands of the clock randomly
 function reset() {
   for (var i = 0; i < 9; i++) {
-    movers[i] = new Mover(random(0.5, 3), 40+i*70, 0);
+    hands[i] = new Mover(random(0.5, 3), 40+i*70, 0);
   }
 }
 
@@ -98,29 +102,26 @@ function Mover(m,x,y) {
 }
 
 // Newton's 2nd law: F = M * A
-// or A = F / M
 Mover.prototype.applyForce = function(force) {
   var f = p5.Vector.div(force,this.mass);
   this.acceleration.add(f);
 };
 
 Mover.prototype.update = function() {
-  // Velocity changes according to acceleration
   this.velocity.add(this.acceleration);
   // position changes by velocity
   this.position.add(this.velocity);
-  // We must clear acceleration each frame
   this.acceleration.mult(0);
 };
 
 Mover.prototype.display = function() {
-  stroke(0);
-  strokeWeight(1);
-  fill(232,127);
-  rect(this.position.x,this.position.y,this.mass*16,this.mass*16);
+  stroke("#ccaaa2");
+  strokeWeight(2);
+  //fill(,127);
+  quad(this.position.x,this.position.y,this.mass*16,this.mass*16);
 };
 
-// Bounce off bottom of window
+// Bounce off bottom accordingly
 Mover.prototype.checkEdges = function() {
   if (this.position.y > (height - this.mass*8)) {
     // A little dampening when hitting the bottom
