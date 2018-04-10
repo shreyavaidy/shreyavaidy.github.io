b780b88e49a5896ee89619665598e383014981c4
Shreya Vaidyanathan <shreyavaidy@example.com>
2018-04-10 00:51:59 -0400

    some changes

--- dvproj/FinalProject/chart2.js
+++ dvproj/FinalProject/chart2.js
@@ -3,8 +3,6 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(250);
-  frameRate(30);
-
 }
 
 function draw() {
@@ -17,14 +15,29 @@ function draw() {
   text('Extract sentiment from natural language?',cx,100);
  
   //Drawing the interactive elements to show the stages of the analysis	 
-  noStroke();
+  push();
+  stroke('#2FE1D6');
+  fill('#38C6BD');
   ellipse(cx/2,cy, 30);
   ellipse(cx,cy, 40);
   ellipse(3*cx/2,cy, 50);
+  pop();
 
-  if (dist(mouseX, mouseY, cx/2, cy) < 15) {
+  if (dist(mouseX, mouseY, cx/2, cy) < 10) {
+        push();
+        fill('#3EAED3');
         textSize(20);
-        text('Words are taken', cx,cy+cy/2);
+        text('Words are taken', cx/2,cy+cy/2);
+        pop();
     }
+  
+  if (dist(mouseX, mouseY, cx, cy) < 20) {
+        push();
+        fill('#3EAED3');
+        textSize(20);
+        text('Bag of Words algorithm', cx,cy+cy/2);
+        pop();
+    }
+
    
 }
