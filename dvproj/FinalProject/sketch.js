b2e9eb2507cd36309b392f480fdc8efc46a2d2b9
Shreya Vaidyanathan <shreyavaidy@example.com>
2018-04-11 09:24:03 -0400

    basic outline
    Chart 1 & 2

--- dvproj/FinalProject/sketch.js
+++ dvproj/FinalProject/sketch.js
@@ -1,9 +1,10 @@
 // An array of stripes
 var stripes = [];
+var myFont1;
 
 function setup() {
-  var cnv1 = createCanvas(windowWidth, 100);
-  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  var cnv1 = createCanvas(windowWidth, 200);
+  // Move the canvas to be inside <div id="sketch2-holder">.
   cnv1.parent('sketch2-holder');
 
   // Initialize all Stripe objects
@@ -12,76 +13,20 @@ function setup() {
   }
 }
 
+
 function draw() {
-  
-  background('#BCE3C8');
+  background('#48466D');
   // Move and display all Stripe objects
   for (var i = 0; i < stripes.length; i ++ ) {
     // Check if mouse is over the Stripe
     stripes[i].rollover(mouseX,mouseY); // Passing the mouse coordinates into an object.
     stripes[i].move();
     stripes[i].display();
-  } 
-}
-
-// we've imported the @trainorpj/sentiment library
-var _d = d3,
-    scaleThreshold = _d.scaleThreshold,
-    scaleOrdinal = _d.scaleOrdinal;
-
-
-var analyze = function analyze(phrase) {
-  var val = sentiment(phrase);
-  return {
-    comparative: val.comparative,
-    negative: val.negative,
-    positive: val.positive
-  };
-};
-
-var emojis = ["^_^", ":D", ":|", ":(", ">.<"];
-var colors = ["#2979FF", "#81D4FA", "#EDE7F6", "#F48FB1", "#E53935"];
-
-var emojiColor = scaleOrdinal().domain(emojis).range(colors);
-
-var emojiSentiment = scaleThreshold().domain([-3, -1, 1, 3, 5]).range([].concat(emojis).reverse());
-
-var app = new Vue({
-  el: "#app",
-  data: function data() {
-    return {
-      emojis: emojis,
-      phrase: "I love waffles \uD83D\uDE01....\nbut hate pancakes \uD83D\uDCA9"
-    };
-  },
-
-  computed: {
-    analysis: function analysis() {
-      var _sentiment = sentiment(this.phrase),
-          comparative = _sentiment.comparative,
-          positive = _sentiment.positive,
-          negative = _sentiment.negative;
-
-      return {
-        comparative: comparative.toFixed(2),
-        positive: positive,
-        negative: negative
-      };
-    },
-    bgc: function bgc() {
-      var emoji = emojiSentiment(this.analysis.comparative);
-      var color = emojiColor(emoji);
-      return { backgroundColor: color };
-    }
-  },
-  methods: {
-    changeBgc: function changeBgc(val) {
-      var emoji = emojiSentiment(val);
-      var color = emojiColor(emoji);
-      this.bgc.backgroundColor = color;
-    },
-    getBgc: function getBgc(emj) {
-      return emojiColor(emj);
-    }
   }
-});
+
+  fill('#FAE3E3');
+  textStyle(BOLD);
+  textSize(30);
+  textFont('Open Sans');
+  text("Buy the Rumour, Sell the News", windowWidth/2+100, 150); 
+}
