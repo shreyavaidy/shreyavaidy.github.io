a5aba9c9b8b72a8a84a9f167bf8bb4167b0937f5
Shreya Vaidyanathan <shreyavaidy>
2018-05-04 17:27:01 -0400

    lines show

--- dvproj/FinalProject/hypothesis/compare_withval.js
+++ dvproj/FinalProject/hypothesis/compare_withval.js
@@ -10,6 +10,26 @@ function preload() {
       'header');
 }
 
+function setup(){
+  createCanvas(windowWidth, 600);
+
+  button1 = createButton('>');
+  button1.position(550, 50);
+  button1.mousePressed(nextState);
+
+  button2 = createButton('<');
+  button2.position(500, 50);
+  button2.mousePressed(backState);
+}
+
+function backState() {
+    window.location.href = "chart4.html";
+}
+
+function nextState() {
+  window.location.href = "finaldemo.html";
+}
+
 // using a p5js table object, return an object having
 // the values of the given column, plus the minimum value
 // and maximum value from that column
@@ -30,16 +50,24 @@ function mouseInBounds(x1, y1, x2, y2){
 }
 
 function draw() { 
-
-  createCanvas(windowWidth, 600);
-  
-
-  console.log(data.getRowCount());
-  console.log(data.columns);
-  
   background('#fafae5');
   fill('#252525');
   stroke(255);
+  //console.log(data.getRowCount());
+  //console.log(data.columns);
+  
+
+
+  //fill('white');
+  //Make lines and labels to show
+  ellipse(200, 45, 10, 10);
+  textSize(15);
+  text("X Axis: Date", 50, 50);
+  text("-------- NIFTY",220, 50);
+  fill(255, 128, 128);
+  stroke(255, 128, 128);
+  ellipse(200, 70, 10, 10);
+  text("------- Pharma",220, 75);
   
   // fetch values and min/max for dates
   dates = data.getColumn("Date");
@@ -64,7 +92,6 @@ function draw() {
   let pharma = colValsMinMax(data, "Pharma");
   console.log(pharma.min);
   console.log(pharma.max);
-
 
   push();
   //moving the origin to the bottom left corner
@@ -107,7 +134,6 @@ function draw() {
   pop();
 
   stroke(255,255,255);
-
   for(var i = 1; i<data.getRowCount(); i++){
     
      //y position is NIFTY, y2 is Pharma; x position is date
@@ -119,34 +145,24 @@ function draw() {
     let prev_y2 = map(pharma.values[i-1], pharma.min, pharma.max, 100, 400);
     let ypos2 = map(pharma.values[i], pharma.min, pharma.max, 100, 400);
 
-    if(mouseInBounds(prev_x-5,prev_y-5,prev_x+5,prev_y+5)){
+    if(mouseInBounds(prev_x-10,prev_y-10,prev_x+10,prev_y+10)){
       //console.log("In bounds");
-      rect(xpos+50, ypos+50, 250, 55, 20);
+      rect(700, 150, 250, 55, 20);
       fill('black');
-      textSize(10);
-      text("Date -"+ date[i], xpos+60, ypos+80);
-      text("Value of NIFTY -"+ypos, xpos+65, ypos+75);
+      noStroke();
+      textSize(12);
+      //text("Date -"+ date[i], 720, 160);
+      text("Value of NIFTY: -"+ypos, 720, 180);
     }
 
-    if(mouseInBounds(prev_x-5,prev_y2-5,prev_x+5,prev_y2+5)){
+    if(mouseInBounds(prev_x-10,prev_y2-10,prev_x+10,prev_y2+10)){
       //console.log("In bounds");
-      rect(xpos2+50, ypos2+50, 250, 55, 20);
+      rect(700, 150, 250, 55, 20);
       fill('black');
-      textSize(10);
-      text("Value of Pharma -"+ypos2, xpos2+65, ypos2+75);
+      textSize(12);
+      text("Value of Pharma -"+ypos2, 720, 180);
     }
   }
-
-  //fill('white');
-  //Make lines and labels to show
-  ellipse(200, 45, 10, 10);
-  textSize(15);
-  text("X Axis: Date", 50, 50);
-  text("-------- NIFTY",220, 50);
-  fill(255, 128, 128);
-  stroke(255, 128, 128);
-  ellipse(200, 70, 10, 10);
-  text("------- Pharma",220, 75);
 }
 
 
